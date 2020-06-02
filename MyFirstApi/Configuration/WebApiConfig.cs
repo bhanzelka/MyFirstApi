bc1e380a962ce6e047eaa0c825999f1b9d3fec0f
Braxden Hanzelka <23laxstar@gmail.com>
2020-06-01 19:47:21 -0600

    Add project files.

---
+++ MyFirstApi/Configuration/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MyFirstApi.Configuration
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Atribute Routing
+           // config.MapHttpAttributeRoutes();
+
+
+           /* config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );*/
+
+            // Convention-based routing
+            config.Routes.MapHttpRoute(
+               name: "DefaultApi",
+               routeTemplate: "api/{controller}/{identiier}",
+               defaults: new {controller="Values", identifier = RouteParameter.Optional}
+               );
+        }
+    }
+}
---
+++ MyFirstApi/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MyFirstApi.Controllers
+{
+    [RoutePrefix("api/Values")]
+    public class ValuesController : ApiController
+    {
+        private List<string> _values = new List<string>
+        {
+            "one",
+            "two",
+            "three"
+        };
+
+        [HttpGet]
+        [Route("")]
+        public List<string> getValues()
+        {
+            return _values;
+        }
+
+        [HttpGet]
+        [Route("{identifier}")]
+        public string GetValue(int id)
+        {
+            return _values[id];
+        }
+
+        [HttpPost]
+        [Route("")]
+        public int PostValue(ValuesRequestBody request)
+        {
+            _values.Add(request.Value);
+            return _values.Count - 1;
+        }
+
+        [HttpPut]
+        [Route("{identifier}")]
+        public int PutValue([FromUri]int identifier, [FromBody]ValuesRequestBody request)
+        {
+            _values[identifier] = request.Value;
+            return identifier;
+        }
+
+        [HttpDelete]
+        [Route("{identifier}")]
+        public void DeleteValue(int identifier)
+        {
+            _values.RemoveAt(identifier);
+        }
+        public class ValuesRequestBody
+        {
+            public string Value { get; set; }
+        }
+    }
+}
